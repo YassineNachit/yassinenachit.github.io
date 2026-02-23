9b7a40cebdeedf1d41d89295656234817195e202
YassineNachit <168003003+YassineNachit@users.noreply.github.com>
2026-02-23 14:33:11 +0000

    Deploying to gh-pages from @ YassineNachit/yassinenachit.github.io@aee6ae1d1a83e37121ea8b821a4f72d4110ad216 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "",
+          description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";

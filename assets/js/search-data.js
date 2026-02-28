b93e3c9f9a876048550104dfe8a77df6934b0a76
YassineNachit <168003003+YassineNachit@users.noreply.github.com>
2026-02-28 14:38:28 +0000

    Deploying to gh-pages from @ YassineNachit/yassinenachit.github.io@c94880785ff171160aabeb40d7fd082e43b3ba79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/Teaching:/";
+            window.location.href = "/teaching/";
           },
         },{id: "post-a-post-with-plotly-js",
         
